b4c79a36eba4ddfddbc0f2c4078291802a2106f3
Muhammad Talha Shaikh <m-talha-shaikh@protonmail.com>
2024-12-06 16:05:04 +0500

    update axios config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Add axios as an external dependency if needed
+      external: ['axios'],
+    },
+  },
 })
